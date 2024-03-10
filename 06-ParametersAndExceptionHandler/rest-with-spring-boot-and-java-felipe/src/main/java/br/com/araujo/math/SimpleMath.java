60f4cf98628bcdaa37e50014762db76bec90fa3a
Felipe Araujo <felipecma.araujo@gmail.com.br>
2024-03-10 02:55:00 -0300

    first commit

---
+++ 06-ParametersAndExceptionHandler/rest-with-spring-boot-and-java-felipe/src/main/java/br/com/araujo/math/SimpleMath.java
@@ -0,0 +1,41 @@
+package br.com.araujo.math;
+
+
+public class SimpleMath {
+	
+
+	public Double sum(double numberOne,double numberTwo){
+		
+		return  numberOne + numberTwo;
+	}
+	
+	
+	public Double sub(double numberOne,double numberTwo) {
+		
+		return  numberOne - numberTwo;
+	}
+	
+	
+	public Double mult(double numberOne,double numberTwo){
+		
+		return  numberOne * numberTwo;
+	}
+	
+	
+	public Double div(double numberOne,double numberTwo){
+		
+		return  numberOne / numberTwo;
+	}
+	
+	public Double med(double numberOne,double numberTwo){
+		
+		return (numberOne + numberTwo) / 2;
+	}
+	
+	
+	public Double sqrt(double number){
+		
+		return Math.sqrt(number);
+	}
+	
+}
