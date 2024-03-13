37891f715ed531961ddbfd80035256545323f73a
Felipe Araujo <felipecma.araujo@gmail.com.br>
2024-03-12 22:43:31 -0300

    First Commit

---
+++ 11-Migration-with-Flyway/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,11 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
+(1,'São Paulo','Ayrton','Male','Senna'),
+(2,'Anchiano - Italy','Leonardo','Male','da Vinci'),
+(4,'Porbandar - India','Indira','Female','Gandhi'),
+(5,'Porbandar - India','Mahatma','Male','Gandhi'),
+(7,'Kentucky - US','Muhammad','Male','Ali'),
+(9,'Mvezo - Soth Africa','Nelson','Male','Mandela'),
+(10,'Mvezo - Soth Africa','Nelson','Male','Mandela'),
+(11,'Mvezo - Soth Africa','Nelson','Male','Mandela'),
+(12,'Smiljan - Croat','Nikola','Male','Tesla'),
+(13,'Smiljan - Croat','Nikola','Male','Tesla');
